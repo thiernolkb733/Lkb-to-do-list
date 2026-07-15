bf1197aaeb2a8116f09289106974454720984114
Thierno Mamadou Diallo <thiernomamadou733@gmail.com>
2026-07-15 15:50:32 +0000

    Mises à jour des elements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const taskInput = document.querySelector("#taskInput");
 const addBtn = document.querySelector("#addBtn");
 const taskList = document.querySelector("#taskList");
 const percentage = document.querySelector("#percentage");
-const taskCounter = document.querySelector("#task-counter");
+const taskCounter = document.querySelector(".task-counter");
 const progress = document.querySelector(".progress");
 
 
@@ -20,7 +20,21 @@ function ajouterTache(){
 
     li.classList.add("task");
     const checkbox = document.createElement("input");
+
     checkbox.type = "checkbox";
+
+    checkbox.addEventListener("change", function(){
+
+        if (checkbox.checked){
+            li.classList.add("completed");
+        }else{
+            li.classList.remove("completed");
+        }
+
+        updateStats();
+
+    });
+
     const titre = document.createElement("span");
     titre.textContent = texte;
     titre.classList.add("task-title");
@@ -39,12 +53,37 @@ function ajouterTache(){
 
     taskList.appendChild(li);
 
-
-
-
-
     taskInput.value = "";
 
+    updateStats();
+
+
+    
+
+}
+
+
+
+function updateStats(){
+
+    const tasks = document.querySelectorAll(".task");
+    const completedTasks = document.querySelectorAll(".task.completed");
+    const total = tasks.length;
+    const completed = completedTasks.length;
+
+    let percent = 0;
+
+    if (total > 0){
+        percent = Math.round((completed/total)* 100)
+    }
+
+    percentage.textContent = percent + "%";
+
+    taskCounter.textContent = completed + "/" + total + " completées";
+
+    progress.style.width = percent + "%";
+
+    
 }
 
 
